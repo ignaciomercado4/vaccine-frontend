9d1e1f38e35f72c0dee42b5233569c0672a98353
Ignacio Mercado <igmercado46@gmail.com>
2024-12-28 18:12:25 -0300

    feat: sort of a signup made

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+
+async function handleSignup() {
+    let $inputs = document.querySelectorAll('input');
+
+    let data = {
+        "name": $inputs[0].value,
+        "email": $inputs[1].value,
+        "password": $inputs[2].value
+    }
+
+    axios.post("http://localhost:8080/api/signup", data, {headers: {"Content-Type": "application/json"}})
+    .then(function (response) {
+        console.log(response);
+        })
+}
+
 const Signup = () => {
     return(
         <div>
-            Signup ("/signup")
+            <form>
+                <input type="text" name="name" id="name" />
+                <input type="text" name="email" id="email" />
+                <input type="text" name="password" id="password" />
+                <button type="button" onClick={handleSignup}>Sign up</button>
+            </form>
         </div>
     );
 }
