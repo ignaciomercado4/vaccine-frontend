f26ee3d531f563aed6e15087d974671c19f44be7
Ignacio Mercado <igmercado46@gmail.com>
2024-12-28 17:00:25 -0300

    feat: sort of a login is made

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,37 @@
+import axios from "axios";
+
+function handleLogin() {
+    let $inputs = document.querySelectorAll('input');
+
+    let data = {
+        "name": $inputs[0].value,
+        "email": $inputs[1].value,
+        "password": $inputs[2].value,
+    }
+
+
+
+    axios.post('http://localhost:8080/api/login', 
+        data, 
+        {headers: {"Content-Type": "application/json"}})
+      .then(function (response) {
+        console.log(response.data.token);
+        sessionStorage.setItem("jwtToken", response.data.token)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+
 const Login = () => {
     return(
         <div>
-            Login ("/login")
+            <form id="form">
+                <input type="text" name="name" id="name" />
+                <input type="email" name="name" id="email" />
+                <input type="text" name="name" id="password" />
+                <button type="button" onClick={handleLogin}>Login</button>
+            </form>
         </div>
     );
 }
