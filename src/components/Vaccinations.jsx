8f5f4b2484a800350d4d62a4beb4d1161600b0c4
Ignacio Mercado <igmercado46@gmail.com>
2025-01-01 20:45:35 -0300

    feat: vaccs. are being fetched

--- src/components/Vaccinations.jsx
+++ src/components/Vaccinations.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Vaccinations() {
+    const [data, setData] = useState({}); 
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const result = await axios.get('http://localhost:8080/api/vaccination', {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${sessionStorage.getItem("jwtToken")}`
+                }
+            });
+            setData(result.data); 
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     return(
         <div>
-            Vaccinations ("/vaccinations")
+            {JSON.stringify(data)}
         </div>
     );
 }
