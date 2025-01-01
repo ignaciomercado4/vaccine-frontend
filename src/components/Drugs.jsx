4c8c46af7f68500362ae2ac1aeee3a1e7d963003
Ignacio Mercado <igmercado46@gmail.com>
2025-01-01 19:46:54 -0300

    feat: now drugs are being fetched and shown

--- src/components/Drugs.jsx
+++ src/components/Drugs.jsx
@@ -1,9 +1,46 @@
-const Drugs = () => {
-    return(
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Drugs() {
+    const [data, setData] = useState({}); 
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const result = await axios.get('http://localhost:8080/api/drugs', {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${sessionStorage.getItem("jwtToken")}`
+                }
+            });
+            setData(result.data); 
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    return (
         <div>
-            Drugs ("/drugs")
+            {data.drugs ? (
+                <ul>
+                    {data.drugs.map((drug, index) => (
+                        <li key={index}>
+                            <strong>Name:</strong> {drug.name} <br />
+                            <strong>Approved:</strong> {drug.approved ? "Yes" : "No"} <br />
+                            <strong>Min Dose:</strong> {drug.minDose} <br />
+                            <strong>Max Dose:</strong> {drug.maxDose} <br />
+                            <strong>Available At:</strong> {new Date(drug.availableAt).toLocaleDateString()}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
     );
 }
 
-export default Drugs
+export default Drugs;
