e1dc7d39c92bb02b51c9dbbea1142b52f8712266
Ignacio Mercado <igmercado46@gmail.com>
2025-01-04 01:14:31 -0300

    feat: better protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Layout from "./layouts/Layout";
-import Home from "./components/Home"
-import Drugs from "./components/drugs/Drugs"
+import Home from "./components/Home";
+import Drugs from "./components/drugs/Drugs";
 import Signup from "./components/users/Signup";
 import Login from "./components/users/Login";
 import Vaccinations from "./components/vaccinations/Vaccinations";
 import { verifySession } from "./utils/auth";
+
+function ProtectedRoute({ children }) {
+  const isAuthenticated = verifySession();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />; 
+  }
+
+  return children; 
+}
 
 function App() {
   return (
@@ -16,8 +26,22 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Home />} />
-          <Route path="/drugs" element={verifySession() ? <Drugs /> : <Login />} />
-          <Route path="/vaccinations" element={verifySession() ? <Vaccinations /> : <Login />} />
+          <Route
+            path="/drugs"
+            element={
+              <ProtectedRoute>
+                <Drugs />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/vaccinations"
+            element={
+              <ProtectedRoute>
+                <Vaccinations />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Layout>
     </Router>
